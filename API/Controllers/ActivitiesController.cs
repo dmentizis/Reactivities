0dd568b81b044e16903d400f4451a8ec1df54b2b
dmentizis <dmentizis@gmail.com>
2025-08-03 19:39:27 +0300

    Code cleanup and controller refactoring

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,43 +2,20 @@ using Application.Activities.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
 
 namespace API.Controllers;
 
-// Using primary constructor for Dependency Injection
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
-    #region Old Depenency Injection Method
-    // Old dependency injection method
-    // private readonly AppDbContext context;
-
-    // public ActivitiesController(AppDbContext context)
-    // {
-    //     this.context = context;
-    // }
-    #endregion
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        #region Old Implementation
-        //var activity = await context.Activities.FindAsync(id);
-
-        // if (activity == null) return BadRequest(); // Error 400
-
-
-        //if (activity == null) return NotFound(); // Error 404
-
-        //return activity;
-        #endregion
-
-        return await mediator.Send(new GetActivityDetails.Query { Id = id });
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
