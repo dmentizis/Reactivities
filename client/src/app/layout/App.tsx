0756d48268d049b56d8fe963f019863cf64f4092
dmentizis <dmentizis@gmail.com>
2025-08-05 17:57:21 +0300

    Created activities detail component.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/ActivityDashboard";
 
 function App() {
   // const title = 'Welcome to Reactivities' // Does not remember the value through the life cycle of the component.
@@ -20,13 +21,7 @@ function App() {
     <CssBaseline/>
       <Navbar/>
       <Container maxWidth='xl' sx={{mt: 3}}>
-        <List>
-          {activities.map((activity) => (
-            <ListItem key={activity.id}>
-              <ListItemText>{activity.title}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities}/>
       </Container>
     </>
   )
